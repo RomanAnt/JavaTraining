b6d8dfadbd4336e06a290a3c0492cce61519e3ca
Roman Ant <orlr300@gmail.com>
2013-10-20 13:03:09 +0400

    Masha save probe

---
+++ Masha/src/Masha.java
@@ -0,0 +1,20 @@
+
+public class Masha 
+{
+
+	public static void main(String[] args) 
+	{
+			
+		
+		String grlf = "Masha";
+		String grf = "Masha";
+		if(grlf == grf)grf = "Misha";
+		String grf1 = new String("Nata");
+		String grf2 = new String("Nata");
+		
+		if (grf1==grf2)System.out.println("\n\tEqual");
+		else System.out.println("\n\tNOT equal");
+
+	}
+
+}
