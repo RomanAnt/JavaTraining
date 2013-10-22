b893e2295613cc7ce767e1b60fc360d41e593c85
Roman Ant <orlr300@gmail.com>
2013-10-22 23:26:10 +0400

    MyApplet first saving

---
+++ MyApplet/src/HelloWRLDApplet.java
@@ -0,0 +1,15 @@
+import javax.swing.JApplet;
+import java.awt.Graphics;
+
+public class HelloWRLDApplet extends JApplet 
+{
+	
+	
+	public void paint(Graphics g)
+	{
+
+	g.drawString("Hello World", 50, 100);
+
+	}
+
+}
