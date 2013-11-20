aac913f9124bcdc7aae27dd6a2ce4a0faf6081a5
RomanAnt <orlr300@gmail.com>
2013-11-20 18:53:57 +0400

    Less20v1_work

---
+++ MiLeccion20/src/TestThreads.java
@@ -0,0 +1,16 @@
+
+public class TestThreads {
+
+    public static void main(String[] args) 
+    {
+	ClassMarketNews mn = new ClassMarketNews("Market News");
+	mn.start();
+	
+	Portfolio p = new Portfolio("Portfolio data");
+	p.start();
+	
+	System.out.println("\tTestThreads is finished");
+
+    }
+
+}
