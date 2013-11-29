4f073f1a640b7e942815202e81c363b8e58e1e82
RomanAnt <orlr300@gmail.com>
2013-11-29 18:41:06 +0400

    work29-11-13

---
+++ MiLeccion20/src/KillTheThread.java
@@ -0,0 +1,38 @@
+
+public class KillTheThread 
+{
+    public static void main (String args[])
+    {
+	Portfolio4 p = new Portfolio4("Portfolio data");
+	p.start();
+	
+	p.stopMe();
+	
+    }
+}
+
+
+class Portfolio4 extends Thread
+{
+    private volatile Thread stopMe = Thread.currentThread();
+    
+    public void stopMe()
+    {
+	stopMe = null;
+    }
+    
+    public void run()
+    {
+	while( stopMe == Thread.currentThread())
+	{
+	    try
+	    {
+		System.out.println("\ninside the try");
+	    }
+	    /*catch (InterruptedException e)
+	    {
+		System.out.println(Thread.currentThread().getName() + e.toString());
+	    }*/
+	}
+    }
+}
